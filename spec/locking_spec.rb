69de777e6e4f916f2084eca5fd07272908ca6149
Pedro Nascimento <pnascimento@gmail.com>
2012-11-19 03:14:32 -0200

    Hardcoding OSX max pid, refactoring.

--- spec/locking_spec.rb
+++ spec/locking_spec.rb
@@ -10,13 +10,7 @@ describe 'bitpocket locking' do
   end
 
   it "exits with status 2 when stale lock found" do
-    if RUBY_PLATFORM =~ /darwvin/
-      max_pid = `ps a | grep ps | grep -v grep | awk '{print $1}'`.to_i
-    else
-      max_pid = File.read('/proc/sys/kernel/pid_max').to_i
-    end
-    cat max_pid, local_path('.bitpocket/tmp/lock')
-
+    cat max_pid, local_path('.bitpocket/tmp/lock/pid')
     sync.should exit_with(2)
   end
 
@@ -36,5 +30,12 @@ describe 'bitpocket locking' do
     sync.should succeed
 
     remote_path('.bitpocket/tmp/lock').should_not exist
+  end
+  def max_pid
+    if RUBY_PLATFORM =~ /darwin/
+      99998
+    else
+      File.read('/proc/sys/kernel/pid_max').to_i
+    end
   end
 end

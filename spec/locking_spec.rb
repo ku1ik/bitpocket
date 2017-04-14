91b9ad855e15bb8e21c165770c21ea0e508d847e
Plato Wu <xiwu@hach.com>
2017-04-14 17:10:25 +0800

    Update cp parents replace method for pull
    Remove test case warning.

--- spec/locking_spec.rb
+++ spec/locking_spec.rb
@@ -3,19 +3,19 @@ require 'spec_helper'
 describe 'bitpocket locking' do
   include_context 'setup'
 
-  it "exits with status 1 when other instance is running" do
+  it 'exits with status 1 when other instance is running' do
     cat $$, local_path('.bitpocket/tmp/lock/pid')
 
     sync.should exit_with(1)
   end
 
-  it "exits with status 2 when stale lock found" do
+  it 'exits with status 2 when stale lock found' do
     cat max_pid, local_path('.bitpocket/tmp/lock/pid')
 
     sync.should exit_with(2)
   end
 
-  it "exits with status 3 when can't acquire remote lock" do
+  it 'exits with status 3 when can\'t acquire remote lock' do
     mkdir remote_path('.bitpocket/tmp/lock')
 
     sync.should exit_with(3)
